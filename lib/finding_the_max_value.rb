e30a797ee9c80d73f86a682dfeb14880ecb48f8a
Will Vetter <vetter.william@gmail.com>
2020-09-01 05:04:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,10 +2,10 @@ def find_max_value(array)
   # Add your solution here
   max = 0
 
-  for item in array {
+  for item in array do
     if item > max
       max = item
     end
-  }
+  end
   max
 end
